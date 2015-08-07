573e8ac92d4c567aaad2982c2f3b68b9ae8e17e8
Goran Draskovic <gdraskovic@gmail.com>
2015-08-07 09:41:35 -0500

    5-2

---  lib/router.js
+++  lib/router.js
@@ -1,5 +1,14 @@
 Router.configure({
-  layoutTemplate: "layout"
-  });
+  layoutTemplate: "layout",
+  loadingTemplate: "loading",
+  waitOn: function() {
+    return Meteor.subscribe("posts");
+  }
+});
 
-Router.route("/", {name: "postsList"});
+Router.route("/", {
+  name: "postsList"
+});
+Router.route("/post/:_id", {
+  name:"postPage"
+});
